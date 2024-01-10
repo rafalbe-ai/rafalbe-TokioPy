133a2813322e0fd2292b660afd2bfe91feb58d20
rafalbe-ai <rafalbe@coit.es>
2024-01-10 08:29:04 +0100

    Módulo 4 acabado

--- Pruebas1.py
+++ Pruebas1.py
@@ -1,11 +1,39 @@
-print("Hola")
+{
+    'desayuno': 
+        [
+            {'nombre': 'Fruta fresca', 'calorias': 50, 'categoria': 'Vegetales'}, 
+            {'nombre': 'Huevos revueltos', 'calorias': 200, 'categoria': 'Proteínas'}, 
+            {'nombre': 'Tostadas de pan integral', 'calorias': 100, 'categoria': 'Cereales'}
+        ], 
+    'comida': 
+        [
+            {'nombre': 'Judías verdes', 'calorias': 50, 'categoria': 'Vegetales'}, 
+            {'nombre': 'Ensalada de atún', 'calorias': 200, 'categoria': 'Vegetales'}, 
+            {'nombre': 'Pescado a la plancha', 'calorias': 250, 'categoria': 'Proteínas'}
+        ],
+    'cena': 
+        [
+            {'nombre': 'Frutos secos', 'calorias': 150, 'categoria': 'Vegetales'}, 
+            {'nombre': 'Sopa de verduras', 'calorias': 100, 'categoria': 'Vegetales'}, 
+            {'nombre': 'Tortilla de patatas', 'calorias': 250, 'categoria': 'Proteínas'}
+        ]
+}
 
-def funcion_raa(texto : str):
-    print(texto)
-    
 
+'''Menú diario:
 
-funcion_raa("Immprime esto")
+Desayuno con un total de 350 calorías (máximo de 500)
+- Leche desnatada (50 calorías)
+- Huevos revueltos (200 calorías)
+- Tostadas de pan integral (100 calorías)
 
+Comida con un total de 400 calorías (máximo de 1200)
+- Judías verdes (50 calorías)
+- Ensalada de atún (200 calorías)
+- Arroz integral (150 calorías)
 
-    
+Cena con un total de 450 calorías (máximo de 900)
+- Tortilla de patatas (250 calorías)
+- Sopa de verduras (100 calorías)
+- Queso fresco (100 calorías)
+¿Estás satisfecho con este menú? (s/n) '''
